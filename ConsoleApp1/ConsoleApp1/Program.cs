51807ad57c78570f940831f50a1d2c8934db9fa0
GuilhermeGarcia-pascoa <paschoal2310@gmail.com>
2025-10-08 17:06:56 +0100

    Adicionar o alfabeto ao contrario no depois do resultado

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,20 +11,22 @@ namespace ConsoleApp1
             int maiorNumero = 0;
             for (int i = 0; i < vetor1.Length; i++)
             {
-                Console.WriteLine("De o "+ (i + 1)+ "º numero");
+                Console.WriteLine("De o " + (i + 1) + "º numero");
                 vetor1[i] = int.Parse(Console.ReadLine());
             }
-            for(int i = 1; i < vetor1.Length; i++)
+            for (int i = 1; i < vetor1.Length; i++)
             {
-                if(vetor1[i] > vetor1[maiorNumero])
+                if (vetor1[i] > vetor1[maiorNumero])
                 {
                     maiorNumero = i;
                 }
             }
-        
+
             Console.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
             Console.WriteLine("O maior numero é: " + vetor1[maiorNumero] + " e está na posição: " + (maiorNumero + 1));
+            Console.WriteLine("ZYXWVUSRQPONMLKJIHFEDCBA");
         }
+
 
     }
 }
